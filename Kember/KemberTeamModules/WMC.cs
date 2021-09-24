0c590a4f7f95d2dd8bf1ac18cea42025719ebc5e
igorek240101 <48420254+igorek240101@users.noreply.github.com>
2021-09-24 23:20:22 +0300

    BaseRealisation

---
+++ Kember/KemberTeamModules/WMC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using Kember;
+using System.Linq;
+
+namespace KemberTeamModules
+{
+    public class WMC : IMetric
+    {
+        public object RunMetric(Assembly assembly, object args)
+        {
+            return assembly.GetTypes().ToList().ConvertAll(t => t.GetMethods().Length).ToArray();
+        }
+    }
+}
