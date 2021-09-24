554c4c23351e28bf8deee08310cbdb7f2ce09eda
igorek240101 <48420254+igorek240101@users.noreply.github.com>
2021-09-24 17:09:34 +0300

    EntityModelInit

--- Kember/KemberModel/Model.cs
+++ Kember/KemberModel/Model.cs
@@ -1,9 +1,36 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace KemberBackModule
 {
-    class Model
+    public class User
     {
+        [Key]
+        [Required]
+        public int Key { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string OpenKey { get; set; }
+    }
+
+    public class Log
+    {
+        [Key]
+        [Required]
+        public int Key { get; set; }
+
+        [Required]
+        public virtual User Owner { get; set; }
+
+        [Required]
+        public DateTime TimeMark { get; set; }
+
+        [Required]
+        public string PathToFile { get; set; }
     }
 
 
@@ -11,10 +38,14 @@ namespace KemberBackModule
     {
         public static AppDbContext db = new AppDbContext();
 
+        DbSet<User> Users { get; set; }
+
+        DbSet<Log> Logs { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder
-                .UseSqlite("Filename=JournalDB.db");
+                .UseSqlite("Filename=LogDB.db");
         }
     }
 
