5a040e8a3982b897c4cb52cca16b513374457f00
igorek240101 <48420254+igorek240101@users.noreply.github.com>
2021-10-05 00:50:36 +0300

    NewTest

--- Kember/KemberMetricTest/WMC.cs
+++ Kember/KemberMetricTest/WMC.cs
@@ -11,13 +11,11 @@ namespace KemberMetricTest
 
         Assembly assembly;
         KemberTeamMetrics.WMC wmc = new KemberTeamMetrics.WMC();
-        (string, int)[] metrics;
 
         [SetUp]
         public void Setup()
         {
             assembly = Assembly.GetAssembly(typeof(Class1));
-            metrics = (wmc.RunMetric(assembly, 0b1111010) as (string, int)[]);
         }
 
         [Test]
@@ -27,7 +25,7 @@ namespace KemberMetricTest
                                   (0b00010010, 23), (0b00100000, 20), (0b01000000, 21), (0b01111010, 27)};
             foreach(var value in input)
             {
-                int res = (wmc.RunMetric(assembly, value.Item1) as (string, int)[]).Length;
+                int res = (wmc.RunMetric(assembly, value.Item1) as (string, string, int)[]).Length;
                 Assert.AreEqual(value.Item2, res, value.Item1.ToString());
             }
         }
@@ -35,8 +33,12 @@ namespace KemberMetricTest
         [Test]
         public void PrivateMethodInClass()
         {
-            int res = metrics.FirstOrDefault(t => t.Item1 == "Class1").Item2;
+            (string, string, int)[] metrics = (wmc.RunMetric(assembly, 0b11111010) as (string, string, int)[]);
+            int res = metrics.FirstOrDefault(t => t.Item2 == "Class1").Item3;
             Assert.AreEqual(1, res);
+            metrics = (wmc.RunMetric(assembly, 0b01111010) as (string, string, int)[]);
+            res = metrics.FirstOrDefault(t => t.Item2 == "Class1").Item3;
+            Assert.AreEqual(0, res);
         }
     }
 }
