eeb65e4befeff485c3f0bb4d967ecfacc7aff20f
igorek240101 <48420254+igorek240101@users.noreply.github.com>
2021-10-08 23:05:13 +0300

    init

---
+++ KemberJava/BackModule/src/Cortege.java
@@ -0,0 +1,9 @@
+class Cortege<T, J> {
+    public T item1;
+    public J item2;
+
+    Cortege(T item1, J item2) {
+        this.item1 = item1;
+        this.item2 = item2;
+    }
+}
---
+++ KemberJava/BackModule/src/IMetric.java
@@ -0,0 +1,10 @@
+
+
+public interface IMetric {
+
+    public Object runMetric(Class<?> _class,Object args);
+
+    public Cortege<Class<?>, Object> read(String input);
+
+    public String write(Cortege<Class<?>, Object> output);
+}
