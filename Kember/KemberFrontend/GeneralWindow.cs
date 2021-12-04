013e1848910fbcf99a9a1c96429b9fa95f93ae3e
WearierPuma01 <mr.bochka003@gmail.com>
2021-12-05 00:06:19 +0300

    Фронт

--- Kember/KemberFrontend/GeneralWindow.cs
+++ Kember/KemberFrontend/GeneralWindow.cs
@@ -24,7 +24,7 @@ namespace KemberFrontend
         /// </summary>
         public GeneralWindow() : base(null)
         {
-            this.Caption = "GeneralWindow";
+            this.Caption = "Kember";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
